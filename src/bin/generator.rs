a95aedeae17eb66101bdc5acbf3ed4aeadd07a47
PiotrekPKP <peterpkppeter@gmail.com>
2022-12-03 14:03:06 +0100

    feat: add generator

--- src/bin/generator.rs
+++ src/bin/generator.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use std::env;
+use std::{env, fs};
 
 fn main() {
     dotenv().ok();
@@ -38,8 +38,26 @@ fn main() {
         .text()
         .expect("Failed to parse data!");
 
+    if res.contains("Please don't repeatedly request this endpoint before it unlocks!") {
+        println!("This day is not yet available!");
+        return;
+    }
+
     let path = format!("./src/input/{}/day_{}.txt", year, day);
-    std::fs::write(path, res.trim_end()).expect("Failed to save data!");
+    fs::write(path, res.trim_end()).expect("Failed to save data!");
+
+    generate_boilerplate(day, year);
 
     println!("Data saved!");
+}
+
+fn generate_boilerplate(day: u32, year: u32) {
+    let path = format!("./src/bin/{}_day_{}.rs", year, day);
+    let content = fs::read_to_string("./src/template/day.txt").unwrap();
+
+    let content = content
+        .replace("<DAY>", &day.to_string())
+        .replace("<YEAR>", &year.to_string());
+
+    fs::write(path, content).expect("Failed to save data!");
 }
