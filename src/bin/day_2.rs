784f40c0d58512c28dc57bc50fd79f58100edeeb
PiotrekPKP <peterpkppeter@gmail.com>
2022-12-03 12:40:32 +0100

    feat: add day 2

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -44,7 +44,16 @@ fn parse_figure(str: &str) -> Figure {
     }
 }
 
-fn parse_outcome(figure_1: &Figure, figure_2: &Figure) -> Outcome {
+fn parse_outcome(str: &str) -> Outcome {
+    match str {
+        "X" => Outcome::Lose,
+        "Y" => Outcome::Draw,
+        "Z" => Outcome::Win,
+        _ => unreachable!(),
+    }
+}
+
+fn get_outcome(figure_1: &Figure, figure_2: &Figure) -> Outcome {
     match (figure_1, figure_2) {
         (Figure::Rock, Figure::Rock)
         | (Figure::Paper, Figure::Paper)
@@ -55,6 +64,20 @@ fn parse_outcome(figure_1: &Figure, figure_2: &Figure) -> Outcome {
         (Figure::Rock, Figure::Scissors)
         | (Figure::Paper, Figure::Rock)
         | (Figure::Scissors, Figure::Paper) => Outcome::Lose,
+    }
+}
+
+fn get_figure(figure: &Figure, outcome: &Outcome) -> Figure {
+    match (figure, outcome) {
+        (Figure::Rock, Outcome::Draw)
+        | (Figure::Scissors, Outcome::Win)
+        | (Figure::Paper, Outcome::Lose) => Figure::Rock,
+        (Figure::Paper, Outcome::Draw)
+        | (Figure::Rock, Outcome::Win)
+        | (Figure::Scissors, Outcome::Lose) => Figure::Paper,
+        (Figure::Scissors, Outcome::Draw)
+        | (Figure::Paper, Outcome::Win)
+        | (Figure::Rock, Outcome::Lose) => Figure::Scissors,
     }
 }
 
@@ -69,11 +92,21 @@ fn main() {
             let opponent = parse_figure(opponent);
             let me = parse_figure(me);
 
-            return parse_outcome(&opponent, &me).get_score() as i32 + me.get_score() as i32;
+            return get_outcome(&opponent, &me).get_score() as i32 + me.get_score() as i32;
         })
         .sum::<i32>();
 
-    let second_part = 2;
+    let second_part = aoc
+        .lines
+        .iter()
+        .map(|game_move| {
+            let (opponent, outcome) = game_move.split_once(" ").unwrap();
+            let opponent = parse_figure(opponent);
+            let outcome = parse_outcome(outcome);
+
+            return get_figure(&opponent, &outcome).get_score() as i32 + outcome.get_score() as i32;
+        })
+        .sum::<i32>();
 
     aoc.output(first_part, second_part);
 }
