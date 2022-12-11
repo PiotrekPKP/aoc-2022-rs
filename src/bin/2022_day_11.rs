4141c28dca5403afad66f483f63d3ce128a9362f
PiotrekPKP <peterpkppeter@gmail.com>
2022-12-11 11:02:11 +0100

    feat: broken day 11

---
+++ src/bin/2022_day_11.rs
@@ -0,0 +1,162 @@
+use aoc::*;
+use std::{collections::HashMap, fmt::format, str::FromStr};
+
+const ROUND_AMOUNT: u8 = 20;
+const OLD_KEYWORD: &str = "old";
+
+#[derive(Debug)]
+struct Test {
+    divisible_by: u32,
+    pass: u8,
+    fail: u8,
+}
+
+impl FromStr for Test {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+
+        let test_line = lines.next().unwrap();
+        let pass_line = lines.next().unwrap();
+        let fail_line = lines.next().unwrap();
+
+        let divisible_by = test_line.split(" ").last().unwrap().parse::<u32>().unwrap();
+        let pass = pass_line.split(" ").last().unwrap().parse::<u8>().unwrap();
+        let fail = fail_line.split(" ").last().unwrap().parse::<u8>().unwrap();
+
+        Ok(Test {
+            divisible_by,
+            pass,
+            fail,
+        })
+    }
+}
+
+impl Test {
+    fn run(&self, value: u32) -> u8 {
+        if value % self.divisible_by == 0 {
+            self.pass
+        } else {
+            self.fail
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Monkey {
+    id: u8,
+    starting_items: Vec<u32>,
+    operation: String,
+    test: Test,
+    iterations: u32,
+}
+
+impl FromStr for Monkey {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+
+        let id_line = lines.next().unwrap();
+        let items_line = lines.next().unwrap();
+        let operation_line = lines.next().unwrap();
+
+        let (_, id) = id_line.split_once(" ").unwrap();
+        let id = id.replace(":", "").parse::<u8>().unwrap();
+
+        let (_, items) = items_line.split_once("Starting items: ").unwrap();
+        let items = items
+            .split(", ")
+            .flat_map(|x| x.parse())
+            .collect::<Vec<u32>>();
+
+        let (_, operation) = operation_line.split_once("Operation: new = ").unwrap();
+
+        let (_, test) = s.split_once("Test").unwrap();
+        let test = test.parse::<Test>().unwrap();
+
+        Ok(Monkey {
+            id,
+            starting_items: items,
+            operation: operation.to_string(),
+            test,
+            iterations: 0,
+        })
+    }
+}
+
+impl Monkey {
+    fn run_op(&mut self, value: u32) -> u32 {
+        let mut op = self.operation.split(" ");
+
+        let left_side = op.next().unwrap();
+        let op_type = op.next().unwrap();
+        let right_side = op.next().unwrap();
+
+        let left_side = if left_side == OLD_KEYWORD {
+            value
+        } else {
+            left_side.parse::<u32>().unwrap()
+        };
+        let right_side = if right_side == OLD_KEYWORD {
+            value
+        } else {
+            right_side.parse::<u32>().unwrap()
+        };
+
+        self.iterations += 1;
+
+        return match op_type {
+            "*" => left_side * right_side,
+            "+" => left_side + right_side,
+            _ => unreachable!(),
+        };
+    }
+}
+
+fn main() {
+    let aoc = AdventOfCode::new(11, 2022);
+
+    let input = &aoc.test_content.as_ref().unwrap();
+
+    let mut monkeys = input
+        .split("\n\n")
+        .flat_map(|monkey_str| monkey_str.parse::<Monkey>())
+        .collect::<Vec<Monkey>>();
+
+    let mut monkey_items = HashMap::<u8, Vec<u32>>::new();
+    monkeys.iter().for_each(|m| {
+        monkey_items.insert(m.id, m.starting_items.clone());
+    });
+
+    (0..ROUND_AMOUNT).for_each(|r| {
+        monkeys.iter_mut().for_each(|monkey| {
+            let items = monkey_items.get(&monkey.id).unwrap().clone();
+
+            items.iter().enumerate().for_each(|(k, worry_level)| {
+                let new_value = monkey.run_op(*worry_level) / 3;
+                let pass_to = monkey.test.run(new_value);
+
+                let mut items_without_worry_level = items.clone();
+                items_without_worry_level.remove(k);
+
+                if r < 2 {
+                    debug(&items_without_worry_level);
+                }
+
+                monkey_items
+                    .insert(monkey.id, items_without_worry_level)
+                    .unwrap();
+
+                monkey_items.get_mut(&pass_to).unwrap().push(new_value);
+            });
+        });
+    });
+
+    let first_part = monkeys;
+
+    let second_part = 2;
+
+    aoc.output(first_part, second_part);
+}
